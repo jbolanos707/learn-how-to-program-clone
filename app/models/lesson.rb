4d19f965800d7f7a0a7ef5056686cec27342e6bc
jennifer bolanos & peter <student@epicodus.com>
2015-06-30 16:44:55 -0700

    previous and next functions under construction

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,27 @@
 class Lesson < ActiveRecord::Base
+  before_save :set_number
   belongs_to :section
 
   validates :name, presence: true
-end 
+
+  def next
+      return Lesson.where("number > #{number}").order("number").first
+  end
+
+    #  return Lesson.where("number > ?", self.number).order("number").first another way of writing it with place holder
+    #  #{number} is like self.number
+
+  def previous
+    return Lesson.where("number < #{number}").order("number DESC").first
+  end
+
+  private
+  def set_number
+    max = Lesson.maximum("number")
+    if max
+      self.number = max + 1
+    else
+      self.number = 1
+    end
+  end
+end
